d7c27bfec738f474975ee94506a004b0e44e7bd4
lauty <lautynovato30@gmail.com>
2024-09-15 16:32:43 -0300

    agrego css al formulario y comienzo a utilizar el state

--- components/formulario.jsx
+++ components/formulario.jsx
@@ -1,11 +1,14 @@
 import "./formulario.css"
-
+import { useState } from "react"
 export function formulario(){
+    const [nombre, setnombre]=useState("");
+    const [contraseña, setcontraseña]=useState("");
+    
     return (
     <section>
         <h1>login</h1>
 
-        <form >
+        <form className="formulario">
             <input type="text" />
             <input type="password"/>
             <button> iniciar sesion </button>
