4b20a836527e2c04387d36ded1335a815abdea72
lauty <lautynovato30@gmail.com>
2024-09-15 16:00:38 -0300

    creo pantalla login junto a los componentes que voy a necesitar

---
+++ app/(tabs)/login.jsx
@@ -0,0 +1,12 @@
+import { formulario } from "../../components/formulario"
+
+function app(){
+    return(
+        <div classname="app">
+        
+          <formulario/>  
+        </div>
+    )
+}
+
+export default app
---
+++ components/formulario.jsx
@@ -0,0 +1,15 @@
+import "./formulario.css"
+
+export function formulario(){
+    return (
+    <section>
+        <h1>login</h1>
+
+        <form >
+            <input type="text" />
+            <input type="password"/>
+            <button> iniciar sesion </button>
+        </form>
+    </section>
+    )
+}
