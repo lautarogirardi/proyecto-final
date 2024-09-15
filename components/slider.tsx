e0e752c1a3a443b3640945b16b3ee45341126d58
lauty <lautynovato30@gmail.com>
2024-09-15 19:21:47 -0300

    agregando slider card, agregando componentes,arreglando errores,finalizar login

--- components/slider.tsx
+++ components/slider.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { SlidingCards } from 'react-native-slide-cards';
 import { useRouter } from 'expo-router';
-import EditScreenInfo from '@/components/EditScreenInfo'; 
 
 interface CardData {
   id: number;
@@ -14,8 +13,8 @@ interface CardData {
 export default function TabOneScreen() {
   const router = useRouter();
   const cardsData: CardData[] = [
-    { id: 1, title: 'agregar curso', buttonText: 'toca para agregar cursos!', route: '/page1' },
-    { id: 2, title: 'agregar profesores', buttonText: 'toca para agregar profesores!', route: '/page2' },
+    { id: 1, title: 'Card 1', buttonText: 'Go to Page 1', route: '/page1' },
+    { id: 2, title: 'Card 2', buttonText: 'Go to Page 2', route: '/page2' },
     // Agrega más tarjetas según sea necesario
   ];
 
@@ -48,17 +47,17 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 20,
+    marginVertical: 30,
     height: 1,
-    width: '20%',
-    backgroundColor: '#eee', 
+    width: '80%',
+    backgroundColor: '#eee',
   },
   card: {
     width: '80%', // Reduce el ancho de las tarjetas
     padding: 15, // Reduce el padding de las tarjetas
     backgroundColor: '#fff',
-    borderRadius: 4,
-    marginVertical: 4,
+    borderRadius: 10,
+    marginVertical: 10,
     alignItems: 'center',
   },
   cardTitle: {
@@ -66,7 +65,11 @@ const styles = StyleSheet.create({
     marginBottom: 8, // Reduce el margen inferior del título
   },
   buttonContainer: {
-    width: '30%', // Ajusta el ancho del contenedor del botón
-    marginTop: 10, // Ajusta el margen superior del contenedor del botón
+    width: '60%', // Ajusta el ancho del contenedor del botón
+    marginTop: 5, // Ajusta el margen superior del contenedor del botón
+  },
+  button: {
+    paddingVertical: 5, // Reduce el padding vertical del botón
+    paddingHorizontal: 10, // Reduce el padding horizontal del botón
   },
 });
