30ad18656334862c2f4a3d60c2f3540aafd530d1
lauty <lautynovato30@gmail.com>
2024-09-15 15:44:54 -0300

    arreglando errores que bloqueaban inicio del sv

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,12 @@
 import { StyleSheet } from 'react-native';
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import Slider from '@react-native-community/slider'; // Importar desde react-native-slider
+import { Text, View } from '@/components/Themed'; 
+import EditScreenInfo from '@/components/EditScreenInfo'; 
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>EPET N20</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      
-      
+      <View style={styles.separator} />
     </View>
   );
 }
@@ -28,10 +25,6 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
-  },
-  slider: {
-    width: '80%',
-    height: 40,
-    marginVertical: 20,
+    backgroundColor: '#eee', 
   },
 });
--- components/Themed.tsx
+++ components/Themed.tsx
@@ -1,7 +1,4 @@
-/**
- * Learn more about Light and Dark modes:
- * https://docs.expo.io/guides/color-schemes/
- */
+
 
 import { Text as DefaultText, View as DefaultView } from 'react-native';
 
